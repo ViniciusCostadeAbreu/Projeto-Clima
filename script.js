b6b5a931af906f62461b040c49ff366a107b5eb5
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2026-01-20 18:31:16 +0000

    criação da requisição para API

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let txtcid = window.document.getElementById('cid')
 let botao = window.document.getElementById('btn-buscar')
 let res = window.document.querySelector('section#clima')
+const API_KEY = 'SUA_API_KEY_AQUI'
+const URL_BASE = 'https://api.openweathermap.org/data/2.5/weather'
+
 
 botao.addEventListener("click",buscarCidade)
 
@@ -19,7 +22,9 @@ function buscarCidade(){
         mensagem("Por favor, informe uma cidade valida")
         txtcid.value = ''
         txtcid.focus()
-    }else{   
+        return
+    }else{ 
+        console.log(`${URL_BASE}?q=${encodeURIComponent(cidade)}&appid=${API_KEY}&units=metric&lang=pt_br`)
         res.innerHTML = `Buscando clima para <strong>${txtcid.value}...</strong>`  
     }
 }
