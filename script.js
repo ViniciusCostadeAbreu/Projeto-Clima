13e8f19014831e80c98c225c64fb49ee1e7897d4
Vinicius Costa de Abreu <vinicius.vc67@gmail.com>
2026-01-21 18:12:08 +0000

    finalização da exibição da previsão de tempo na tela consumindo a API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let txtcid = window.document.getElementById('cid')
 let botao = window.document.getElementById('btn-buscar')
 let res = window.document.querySelector('section#clima')
-const API_KEY = 'SUA_API_KEY_AQUI'
+const API_KEY = '8b3066d39459bb6f8a13b787b2be7391'
 const URL_BASE = 'https://api.openweathermap.org/data/2.5/weather'
 
 
@@ -14,7 +14,7 @@ txtcid.addEventListener('keypress', function(e) {
 })
 
 
-function buscarCidade(){
+async function buscarCidade(){
 
     let cidade = txtcid.value.trim()
 
@@ -24,9 +24,38 @@ function buscarCidade(){
         txtcid.focus()
         return
     }else{ 
-        console.log(`${URL_BASE}?q=${encodeURIComponent(cidade)}&appid=${API_KEY}&units=metric&lang=pt_br`)
-        res.innerHTML = `Buscando clima para <strong>${txtcid.value}...</strong>`  
+        res.innerHTML = `Buscando clima para <strong>${cidade}...</strong>`
+
+        let url = `${URL_BASE}?q=${encodeURIComponent(cidade)}&appid=${API_KEY}&units=metric&lang=pt_br`
+        
+        try {
+            const response = await fetch(url)
+
+            if(!response.ok){
+                throw new Error(`Não foi possivel encontrar a cidade "${cidade}"`)
+            }
+
+            const data = await response.json()
+            mostrarClima(data) 
+        } catch (error) {
+            res.innerHTML = 'Erro ao buscar dados. Tente novamente.'//error.message
+        };         
     }
+}
+
+function mostrarClima(data) {
+    
+    const iconCode = data.weather[0].icon
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+    res.innerHTML = `
+        <h2>${data.name}</h2>
+        <img src="${iconUrl}" alt="${data.weather[0].description}">
+        <p>${data.main.temp}°C</p>
+        <p>${data.weather[0].description}</p>
+    `
+
+    return res.innerHTML
 }
 
 function mensagem(txt){
